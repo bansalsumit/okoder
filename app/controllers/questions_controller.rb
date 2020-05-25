d869236506f1eb6f7bfb81ad40bf7dd1c95ce442
sumit bansal <sumit.bansal@loconav.com>
2020-05-25 15:26:57 +0530

    Follow topic optimization done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,15 +4,18 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
     @answers = @question.answers
     unless @question.present?
-      flash.now[:alert] = "Your book was not found"
       redirect_to root_path and return
     end
   end
 
   def follow_topic
     @question = Question.find(params[:id])
-    current_user.topics << @question.topic
-    if current_user.save
+    if current_user.topics.exists?(@question.topic)
+      flash.now[:alert] = "You already followed this topic"
+      redirect_to question_path(@question)
+    else
+      current_user.topics << @question.topic
+      current_user.save
       redirect_to root_path
     end
   end
